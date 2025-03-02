a330388b078101a2d9fa43a4bac0d17c3bc28ab8
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2025-03-02 14:20:20 -0600

    05 detail page for properties

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -10,12 +10,16 @@ const PropertyListItem = () => {
                     sizes="(max-width: 768px) 768px, (max-width: 1200px: 768px, 768px)"
                     className="hover:scale-110 object-cover transition h-full w-full"
                     alt="Beach house"
-                    />
+                />
             </div>
+
             <div className="mt-2">
                 <p className="text-lg font-bold">Property name</p>
             </div>
-            <div className="text-sm text-gray-500"><strong>$200</strong> per night</div>
+
+            <div className="mt-2">
+                <p className="text-sm text-gray-500"><strong>$200</strong> per night</p>
+            </div>
         </div>
     )
 }
