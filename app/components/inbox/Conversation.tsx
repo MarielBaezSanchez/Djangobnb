4184b9bd1c90812fb5a1f68430f67a7d7aac4a23
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2025-03-07 18:25:38 -0600

    Inbox page (static)

---
+++ app/components/inbox/Conversation.tsx
@@ -0,0 +1,10 @@
+const Conversation = () => {
+    return(
+        <div className="px-6 py-6 border border-gray-600 rounded-xl">
+            <p className="mb-6 tetx-xl">Mariel</p>
+
+            <p className="text-airbnb-dark">¡Miriam, pon la tetera!</p>
+        </div>
+    );
+}
+export default Conversation;
