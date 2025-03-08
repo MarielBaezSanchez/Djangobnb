396950b8f97be7598dc6b274da73bae07bd5bc08
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2025-03-08 17:27:59 -0600

    login

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,45 +1,76 @@
-'use client';
- 
- import useLoginModal from "@/app/hooks/useLoginModal";
- import Modal from "./Modal";
- import CustomButton from "../forms/CustomButton";
- 
- const LoginModal = () => {
-     const loginModal = useLoginModal()
- 
-     const content = (
-         <>
-             <h2 className="mb-6 text-2xl">
-                 Wenas wenas, registrate
-             </h2>
-             <form className="space-y-4">
-                 <input
-                     placeholder="Your E-mail"
-                     type="email"
-                     className="w-full h-[54px] px-4 border border-gray-100 rounded-xl"
-                 />
-                 <input
-                     placeholder="Your Password"
-                     type="password"
-                     className="w-full h-[54px] px-4 border border-gray-100 rounded-xl"
-                 />
-                 <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
-                     Ingresa bien los campos imbecil
-                 </div>
-                 <CustomButton
-                     label="Submit"
-                     onClick={() => console.log("tas dentro")}
-                 />
-             </form>
-         </>
-     )
-     return (
-         <Modal
-             isOpen={loginModal.isOpen}
-             close={loginModal.close}
-             label="Log in"
-             content={content}
-         />
-     )
- }
- export default LoginModal;
+import useLoginModal from "@/app/hooks/useLoginModal";
+import Modal from "./Modal";
+import CustomButton from "../forms/CustomButton";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import apiService from "@/app/services/apiService";
+import { handleLogin } from "@/app/lib/actions";
+
+const LoginModal = () => {
+    const router = useRouter()
+    const loginModal = useLoginModal()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errors, setErrors] = useState<string[]>([])
+
+    const submitLogin = async () => {
+        const formData = {
+            email: email,
+            password: password
+        }
+
+        const response = await apiService.post('/api/auth/login/', JSON.stringify(formData))
+        if (response.access) {
+            handleLogin(response.user.pk, response.access, response.refresh)
+            loginModal.close()
+            router.push('/')
+        } else {
+            setErrors(response.non_field_errors)
+        }
+    }
+
+    const content = (
+        <>
+            <h2 className="mb-6 text-2xl">
+                registraishon porfis
+            </h2>
+            <form 
+                action={submitLogin}
+                className="space-y-4"
+            >
+                <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Your E-mail"
+                    type="email"
+                    className="w-full h-[54px] px-4 border border-gray-100 rounded-xl"
+                />
+                <input
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Your Password"
+                    type="password"
+                    className="w-full h-[54px] px-4 border border-gray-100 rounded-xl"
+                />
+                {errors.map((error, index) => (
+                    <div
+                        key={`error-${index}`}
+                        className="p-5 bg-airbnb text-white rounded-xl opacity-80">
+                        {error}
+                    </div>
+                ))}
+                <CustomButton
+                    label="Submit"
+                    onClick={submitLogin}
+                />
+            </form>
+        </>
+    )
+    return (
+        <Modal
+            isOpen={loginModal.isOpen}
+            close={loginModal.close}
+            label="Log in"
+            content={content}
+        />
+    )
+}
+export default LoginModal;
