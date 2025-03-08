1dedb254ca5cea8afb338d95f999f1a927e5d502
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2025-03-08 00:34:19 -0600

    Create api service file (And user it in the properties list)

---
+++ app/services/apiService.ts
@@ -0,0 +1,27 @@
+import { resolve } from "path";
+ import { json } from "stream/consumers";
+ 
+ const apiService = {
+     get: async function (url: string): Promise<any> {
+         console.log('get', url)
+ 
+         return new Promise ((resolve, reject) => {
+             fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`,{
+                 method: 'GET',
+                 headers: {
+                     'Accept': 'application/json',
+                     'Content-Type': 'application/json',
+                 }
+             })
+             .then(response => response.json())
+             .then((json) => {
+                 console.log('response', json)
+                 resolve(json)
+             })
+             .catch((error) => {
+                 console.log('error', error)
+                 reject(error)
+             })
+         })
+     }
+ }
